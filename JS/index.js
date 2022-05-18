89faf4f42a90df3df8249a9306b254fc0a115bc3
AliVeli7 <100687859+AliVeli7@users.noreply.github.com>
2022-05-18 15:21:27 +0400

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+// // function find(word) {
+// //     var count =0;
+// //     var isavailabe= false;
+// //         for (var i = 0; i < word.length; i++) {
+// //             for (var j = 0; j < word.length; j++) {
+// //                 if (word[i]==word[j]) {
+// //                     count++;
+// //                 }
+// //             }
+// //             for (var n = 0; n <=i ; n++) {
+// //                 if (n!==i) {
+// //                     if (word[n]==word[i]) {
+// //                     isavailabe=true;
+// //                 }
+// //                 }
+// //             }
+// //             n=0;
+// //             if (isavailabe!==true) {
+// //                  console.log(word[i] + " sayi " + count);
+// //             } 
+// //             isavailabe=false;
+// //             j=0;f
+// //             count=0;
+    
+// //         }
+        
+// // };
+// // find('kamran');
